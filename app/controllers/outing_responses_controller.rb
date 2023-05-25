eb107a672c4e7f5f898cb978a65c6d1036e81b08
Jordan Hancock <jhancock@chicagobooth.edu>
2023-05-25 07:28:23 +0000

    Better spot to cut

--- app/controllers/outing_responses_controller.rb
+++ app/controllers/outing_responses_controller.rb
@@ -17,15 +17,12 @@ class OutingResponsesController < ApplicationController
     render({ :template => "outing_responses/show.html.erb" })
   end
 
-
   def create
     selected_options = params.fetch("interest")
-    outing = params.fetch("query_outing_id")
+    outing_id = params.fetch("query_outing_id")
     #Participant ID should be better tracked so not having to do this; look into associations
-    invite = @current_user.invitations.find_by(outing_id: outing)
-    # @a = selected_options
-    # @b = outing
-    # @c = invite
+    invite = @current_user.invitations.find_by(outing_id: outing_id)
+
     # render({:template => "outing_responses/temp_dummy.html.erb"})
     selected_options.each do |selection|
       the_outing_response = OutingResponse.new
@@ -39,53 +36,26 @@ class OutingResponsesController < ApplicationController
     #Update participants status
     invite.participant_submitted = true
     invite.save
-    #Updates of counts?
-
+    
+    #Updates of outing status
+    if invite.outing.ready
+       invite.outing.completed = true
+       invite.outing.save
+       the_outing_options = invite.outing.outing_options
+       the_outing_options.each do |option|
+         if option.selected
+           option.all_participants_selected = true
+           option.save
+         end
+       end
+     end
     #if the_outing_response.valid?
     ##Redirect to outing page after participants status is updated
-    redirect_to("/outings/#{outing}", { :notice => "Outing response created successfully." })
+    redirect_to("/outings/#{outing_id}", { :notice => "Outing response created successfully." })
     # else
     #   redirect_to("/outing_responses", { :alert => the_outing_response.errors.full_messages.to_sentence })
     # end
     
   end
 
-  # def create
-  #   the_outing_response = OutingResponse.new
-  #   the_outing_response.option_id = params.fetch("query_option_id")
-  #   the_outing_response.participant_id = params.fetch("query_participant_id")
-  #   the_outing_response.participant_selected = params.fetch("query_participant_selected", false)
-
-  #   if the_outing_response.valid?
-  #     the_outing_response.save
-  #     redirect_to("/outing_responses", { :notice => "Outing response created successfully." })
-  #   else
-  #     redirect_to("/outing_responses", { :alert => the_outing_response.errors.full_messages.to_sentence })
-  #   end
-  # end
-
-  def update
-    the_id = params.fetch("path_id")
-    the_outing_response = OutingResponse.where({ :id => the_id }).at(0)
-
-    the_outing_response.option_id = params.fetch("query_option_id")
-    the_outing_response.participant_id = params.fetch("query_participant_id")
-    the_outing_response.participant_selected = params.fetch("query_participant_selected", false)
-
-    if the_outing_response.valid?
-      the_outing_response.save
-      redirect_to("/outing_responses/#{the_outing_response.id}", { :notice => "Outing response updated successfully."} )
-    else
-      redirect_to("/outing_responses/#{the_outing_response.id}", { :alert => the_outing_response.errors.full_messages.to_sentence })
-    end
   end
-
-  def destroy
-    the_id = params.fetch("path_id")
-    the_outing_response = OutingResponse.where({ :id => the_id }).at(0)
-
-    the_outing_response.destroy
-
-    redirect_to("/outing_responses", { :notice => "Outing response deleted successfully."} )
-  end
-end
